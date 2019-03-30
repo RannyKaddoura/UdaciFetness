8687bbbff143392b254d0072900c0bd0f40eb29f
Ranny <ranny.kaddoura@starticket.ch>
2019-03-30 20:07:52 +0100

    first commit after build the UdaciFetness app

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import AddEntry from './components/AddEntry';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.app}>
+        <AddEntry />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  app: {
+    marginTop: 50
+  }
+})
