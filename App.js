66e6f6bb11db94043cd4a925e42d210fe4a4155d
Ranny <ranny.kaddoura@starticket.ch>
2019-03-31 21:27:24 +0200

    add Styling

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={styles.app}>
+        <View style={{flex: 1, marginTop: 50}}>
           <AddEntry />
         </View>
       </Provider>
